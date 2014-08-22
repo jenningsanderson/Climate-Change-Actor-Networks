7bbb881a64e305111ca2ede527269a4d003af69b
Jennings Anderson <jennings.anderson@colorado.edu>
2014-08-22 09:30:09 -0600

    added edge functionality, when it comes

--- excel_parser.py
+++ excel_parser.py
@@ -9,6 +9,7 @@ import sys, os
 
 from models import *
 
+#
 format = {
 	'name' 		: 2,
 	'id'		: 1,
@@ -18,29 +19,34 @@ format = {
 	'location'	: 8
 }
 
+edge_format = {
+	'from_id'	: 1,
+	'to_id'		: 2,
+	'weight'	: 3,
+}
+
 
 def parse_spreadsheet(file_path):
 	#Open the file for reading
 	wb = px.load_workbook(filename = file_path)
-	ws = wb.get_sheet_by_name(wb.get_sheet_names()[0])
-
-	#Iterate through the first sheet:
-	# for row in 
+	
+	#This is the node sheet
+	ws1 = wb.get_sheet_by_name(wb.get_sheet_names()[0])
 
 	nodes = []
 
-	for sheet_row in range(2,ws.max_row):
+	for sheet_row in range(2,ws1.max_row):
 	# for sheet_row in range(1,10):	
 		
-		if ws.cell(row=sheet_row, column=1) != None: #If no id, don't parse
+		if ws1.cell(row=sheet_row, column=1) != None: #If no id, don't parse
 
 			#Collect values
 			name = ws.cell(row=sheet_row, column=format['name']).value
-			obj_id   = ws.cell(row=sheet_row, column=format['id']).value
-			obj_type = ws.cell(row=sheet_row, column=format['type']).value
-			type_no  = ws.cell(row=sheet_row, column=format['type_no']).value
-			discourse= ws.cell(row=sheet_row, column=format['discourse']).value
-			location = ws.cell(row=sheet_row, column=format['location']).value
+			obj_id   = ws1.cell(row=sheet_row, column=format['id']).value
+			obj_type = ws1.cell(row=sheet_row, column=format['type']).value
+			type_no  = ws1.cell(row=sheet_row, column=format['type_no']).value
+			discourse= ws1.cell(row=sheet_row, column=format['discourse']).value
+			location = ws1.cell(row=sheet_row, column=format['location']).value
 			
 			obj = ClimateChangeActor(id=obj_id, name=name)
 
@@ -50,12 +56,21 @@ def parse_spreadsheet(file_path):
 								discourse	= discourse,
 								location 	= location)
 
-
 			#Conditionally set other values such as Alexa Web Ranking
 
 		nodes.append( obj )
 
+
+	#Now add edges
+
+	# ws2 = wb.get_sheet_by_name(wb.get_sheet_names()[1])
+	
 	edges = []
+
+	# for sheet_row in range(2,ws2.max_row):
+	# 	if ws2.cell(row=sheet_row, column=1) != None: #If no id, don't parse
+	
+
 
 	return nodes, edges
 
