50fe723033fac3c26c97d79216450dc97491ccd1
Jennings Anderson <jennings.anderson@colorado.edu>
2014-08-22 08:18:11 -0600

    updating again

--- excel_parser.py
+++ excel_parser.py
@@ -5,26 +5,8 @@ This module will read the excel document and create instances of nodes, edges to
 
 import openpyxl as px
 import numpy as np
+import sys, os
 
+import models
 
-'''
-Nodes will be instances of this class, specific metrics and methods can be defined 
-here to create node attributes.
-
-Hopefully NetworkX will accept nodes as instances of these classes? Hopefully...
-'''
-
-class ClimateChangeActor:
-
-	def __init__(self, name):
-		self.id = 
-		self.name = name
-
-
-class ActorRelation:
-
-	def __init__(self, from_id, to_id):
-
-		self.from_id = from_id
-		self.to_id = to_id
 
